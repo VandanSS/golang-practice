33584ecdeee9372736c6d0f694f7e74692125359
Vandan Sawant <vanss25a@gmail.com>
2020-09-02 16:11:38 +0530

    removed redundant swap funtion

--- misc/BubbleSort.go
+++ misc/BubbleSort.go
@@ -4,42 +4,30 @@ import (
 	"fmt"
 )
 
-
-func Swap(sli []int, index int) {
-
-	if index > len(sli) {
-		fmt.Println("Error : ArrayIndexOutOfBound")
-	} else {
-		sli[index + 1], sli[index] = sli[index], sli[index + 1]
-	}
-}
-
-
 func BubbleSort(list []int) {
 
-	for i := len(list); i >= 2; i--{
-		for j := 0; j < i-1; j++{
-			if list[j] > list[j + 1]{
-				Swap(list, j)
+	for i := len(list); i >= 2; i-- {
+		for j := 0; j < i-1; j++ {
+			if list[j] > list[j+1] {
+				list[j+1], list[j] = list[j], list[j+1]
 			}
 		}
 	}
 }
 
-
 func main() {
 
 	var integerList []int
 	var num int
-	
+
 	fmt.Println("Enter a sequence of 10 integers:")
 	for i := 0; i < 10; i++ {
 		fmt.Scan(&num)
 		integerList = append(integerList, num)
 	}
-	
+
 	BubbleSort(integerList)
-	for _, value:= range integerList {
-         fmt.Printf("%v ", value)
-    }
+	for _, value := range integerList {
+		fmt.Printf("%v ", value)
+	}
 }
